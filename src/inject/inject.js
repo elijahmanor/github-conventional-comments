0cca49e16eb3234742ccba53b0bed6a14c649264
Elijah Manor <emanor@planview.com>
2021-11-16 08:49:59 -0600

    added tooltip to conventional comment trigger

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,9 +8,11 @@ const getRawComment = (comment) => {
 };
 const updateComment = (comment, label, decorations) => {
     const rawComment = getRawComment(comment.value);
-    comment.value = label ? `${label}${decorations ? ` (${decorations})` : ''}: ${
-        rawComment || ''
-    }` : rawComment;
+    comment.value = label
+        ? `${label}${decorations ? ` (${decorations})` : ''}: ${
+              rawComment || ''
+          }`
+        : rawComment;
 };
 const getLabelAndDecorations = (comment) => {
     const [, label, decorations] = comment.match(/(.*) \((.*)\): (.*)/) || [];
@@ -41,11 +43,13 @@ const createTrigger = ({ comment, controls }) => {
         'http://www.w3.org/1999/xhtml',
         'div'
     );
+    trigger.ariaLabel = 'Conventional Comments';
     trigger.innerHTML = `
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M5.00136 5H18.9986C18.9989 5 18.9991 5 18.9992 5.00001C18.9994 5.00016 18.9997 5.00038 19 5.00066C19 5.00067 19 5.00067 19 5.00068V13.7085H11.9976C11.3708 13.7085 10.7802 14.0024 10.4022 14.5024L8.06345 17.5963L7.6365 15.3371C7.45798 14.3925 6.63263 13.7085 5.67128 13.7085H5V5.00068C5.00001 5.00067 5.00001 5.00067 5.00002 5.00066C5.0003 5.00038 5.00055 5.00016 5.00075 5.00001C5.00093 5 5.00113 5 5.00136 5Z" stroke="currentColor" stroke-width="2"/>
         </svg>`;
-    trigger.className = 'GitHubConventionalComments-trigger';
+    trigger.className =
+        'GitHubConventionalComments-trigger toolbar-item tooltipped tooltipped-sw';
     trigger.addEventListener('click', () => {
         controls.classList.toggle('is-open');
         trigger.classList.toggle('is-open');
